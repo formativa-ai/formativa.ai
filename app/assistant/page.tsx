aaee02727ca84c73e9f7c010032a0f886f8ad2ae
Mateo Ortegon <60627969+blacksheep-git@users.noreply.github.com>
2024-07-03 19:25:03 -0400

    Added career assistant

--- app/assistant/page.tsx
+++ app/assistant/page.tsx
@@ -1,6 +1,19 @@
+"use client";
 
-export default function AssistantPage() {
-    return (
-        <div className="text-white">Assistant</div>
-    );
-}
+import React from "react";
+import styles from "./page.module.css"; // use simple styles for demonstration purposes
+import Chat from "../components/chat";
+import TopNavBar from "@/app/components/TopNavBar";
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+        <TopNavBar />
+      <div className={styles.container}>
+        <Chat />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
--- app/components/TopNavBar.tsx
+++ app/components/TopNavBar.tsx
@@ -27,7 +27,7 @@ export default function TopNavBar() {
                       <div className="flex space-x-4">
                         {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                         <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
-                          Dashboard
+                          Home
                         </a>
                         <a
                             href="/about"
@@ -36,10 +36,10 @@ export default function TopNavBar() {
                           About
                         </a>
                         <a
-                            href="#"
+                            href="/assistant"
                             className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                         >
-                          Projects
+                          Guia de Careera
                         </a>
                         <a
                             href="#"
