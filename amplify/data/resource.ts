99e2b196bb74ee5ece8a3a14547c5b3a9d0f5252
Mateo Ortegon <60627969+blacksheep-git@users.noreply.github.com>
2024-09-20 10:47:49 -0400

    Added Message model and has many messages reltaionship to Chat model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -2,12 +2,24 @@ import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 
 const schema = a.schema({
-  Chat: a
-    .model({
-      threadId: a.string(),
-      // owner: a.string().authorization(allow => [allow.owner().to(['read', 'delete'])])
-    })
-      .authorization(allow => [allow.owner().to(['create', 'read', 'update'])]),
+    // Chat object model to store Messages in a thread, identified by chatId
+    Chat: a
+        .model({
+            threadId: a.string(),
+            messages: a.hasMany('Message', 'chatId'),
+            owner: a.string().authorization(allow => [allow.owner().to(['read', 'delete'])])
+        })
+        .authorization(allow => [allow.owner()]),
+    // Message object belongs to a Chat identified by chatId
+    Message: a
+        .model({
+            chatId: a.id().required(),
+            content: a.string(),
+            role: a.string(),
+            chat: a.belongsTo('Chat', 'chatId'),
+            owner: a.string().authorization(allow => [allow.owner().to(['read', 'delete'])])
+        })
+        .authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
