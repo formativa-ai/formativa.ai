33c755d6776d6989e175d5fb7d9288f2abf3d45b
Mateo Ortegon <60627969+blacksheep-git@users.noreply.github.com>
2024-09-24 19:37:07 -0400

    Added ui components dir to tailwinds

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,13 @@ const config= {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui-components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       animation: {
         shimmer: "shimmer 2s linear infinite",
