b0915282364348e0566e33e45da0bb8e21db0d10
Mateo Ortegon <60627969+blacksheep-git@users.noreply.github.com>
2024-09-25 22:23:29 -0400

    Changed footer copyright text

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer({background}) {
                     Hecho con orgullo en Manizales, Colombia <span className="mx-1">🇨🇴</span>
                 </p>
                 <p className="mt-6 text-center text-xs text-gray-500">
-                    &copy; {new Date().getFullYear()} Formativa AI. Todos los derechos reservados.
+                    &copy; {new Date().getFullYear()} Formativa Technologies. Todos los derechos reservados.
                 </p>
             </div>
         </footer>
